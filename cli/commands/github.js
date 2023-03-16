01bd032b9103aa85c43e65020f4d285a578d786e
kupppo <kupppo@protonmail.com>
2023-03-16 16:47:25 +0000

    cleanup github command

--- cli/commands/github.js
+++ cli/commands/github.js
@@ -5,9 +5,10 @@ const chalk = require('chalk')
 const seedCommand = () => {
   const program = new commander.Command('github')
   program
-    .description('Open Github repository in browser')
+    .description('Open Github in browser')
+    .alias('source')
     .action(async () => {
-      const url = 'https://github.com/dashrando/dashrando.github.io'
+      const url = 'https://github.com/dashrando'
       console.log(chalk.gray('Opening'), chalk.cyan(url))
       await open(url)
     })
