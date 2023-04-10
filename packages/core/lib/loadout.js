b0720b7bd13ec41d48e3b258d1d232229d98e415
kupppo <kupppo@protonmail.com>
2023-04-10 16:40:03 +0100

    refactor loadout to find items via functions

--- packages/core/lib/loadout.js
+++ packages/core/lib/loadout.js
@@ -1,194 +1,115 @@
 import { Item, ItemNames } from "./items";
 
 class Loadout {
-  hasBombs = false;
-  hasMorph = false;
-  hasGravity = false;
-  hasVaria = false;
-  hasHiJump = false;
-  hasSpaceJump = false;
-  hasScrewAttack = false;
-  hasSpringBall = false;
-  hasSpeed = false;
+  ammo = {
+    missilePacks: 0,
+    superPacks: 0,
+    powerPacks: 0,
+  }
+  tanks = {
+    energyTanks: 0,
+    reserveTanks: 0,
+  }
+  inventory = [];
 
-  hasHeatShield = false;
-  hasPressureValve = false;
-  hasDoubleJump = false;
+  constructor(inventory, ammo, tanks) {
+    this.inventory = inventory;
+    this.ammo = ammo;
+    this.tanks = tanks;
+  }
 
-  hasIce = false;
-  hasWave = false;
-  hasCharge = false;
-  hasSpazer = false;
-  hasPlasma = false;
-  hasGrapple = false;
+  has(itemType) {
+    return this.inventory.includes(itemType);
+  }
 
-  missilePacks = 0;
-  superPacks = 0;
-  powerPacks = 0;
+  canDestroyBombWalls() {
+    return this.canPassBombPassages() || this.has(Item.ScrewAttack)
+  }
 
-  energyTanks = 0;
-  reserveTanks = 0;
-  totalTanks = 0;
+  canFly() {
+    return this.canUseBombs() || this.has(Item.SpaceJump);
+  }
 
-  canUseBombs = false;
-  canUsePowerBombs = false;
-  canPassBombPassages = false;
-  canDestroyBombWalls = false;
-  canOpenGreenDoors = false;
-  canOpenRedDoors = false;
-  canOpenYellowDoors = false;
-  canFly = false;
-  canCrystalFlash = false;
+  canUseBombs() {
+    return this.has(Item.Bombs) && this.has(Item.Morph)
+  }
+
+  canUsePowerBombs() {
+    return this.ammo.powerPacks > 0 && this.has(Item.Morph)
+  }
+
+  canPassBombPassages() {
+    return this.canUseBombs() || this.canUsePowerBombs()
+  }
+
+  canOpenGreenDoors() {
+    return this.ammo.superPacks > 0
+  }
+
+  canOpenRedDoors() {
+    return this.ammo.missilePacks > 0 || this.ammo.superPacks > 0
+  }
+
+  canOpenYellowDoors() {
+    return this.ammo.powerPacks > 0 && this.canUsePowerBombs()
+  }
+
+  canCrystalFlash() {
+    return (
+      this.canUsePowerBombs() &&
+      this.ammo.missilePacks >= 2 &&
+      this.ammo.superPacks >= 2 &&
+      this.ammo.powerPacks >= 3
+    )
+  }
+
+  totalTanks() {
+    return this.tanks.energyTanks + this.tanks.reserveTanks
+  }
 
   clone() {
-    let copy = new Loadout();
-
-    copy.hasBombs = this.hasBombs;
-    copy.hasMorph = this.hasMorph;
-    copy.hasGravity = this.hasGravity;
-    copy.hasVaria = this.hasVaria;
-    copy.hasHiJump = this.hasHiJump;
-    copy.hasSpaceJump = this.hasSpaceJump;
-    copy.hasScrewAttack = this.hasScrewAttack;
-    copy.hasSpringBall = this.hasSpringBall;
-    copy.hasSpeed = this.hasSpeed;
-
-    copy.hasHeatShield = copy.hasHeatShield;
-    copy.hasPressureValve = copy.hasPressureValve;
-    copy.hasDoubleJump = copy.hasDoubleJump;
-
-    copy.hasIce = this.hasIce;
-    copy.hasWave = this.hasWave;
-    copy.hasCharge = this.hasCharge;
-    copy.hasSpazer = this.hasSpazer;
-    copy.hasPlasma = this.hasPlasma;
-    copy.hasGrapple = this.hasGrapple;
-
-    copy.missilePacks = this.missilePacks;
-    copy.superPacks = this.superPacks;
-    copy.powerPacks = this.powerPacks;
-
-    copy.energyTanks = this.energyTanks;
-    copy.reserveTanks = this.reserveTanks;
-    copy.totalTanks = this.totalTanks;
-
-    copy.canUseBombs = this.canUseBombs;
-    copy.canUsePowerBombs = this.canUsePowerBombs;
-    copy.canPassBombPassages = this.canPassBombPassages;
-    copy.canDestroyBombWalls = this.canDestroyBombWalls;
-    copy.canOpenGreenDoors = this.canOpenGreenDoors;
-    copy.canOpenRedDoors = this.canOpenRedDoors;
-    copy.canOpenYellowDoors = this.canOpenYellowDoors;
-    copy.canFly = this.canFly;
-    copy.canCrystalFlash = this.canCrystalFlash;
-
-    return copy;
+    return new Loadout(this.inventory, this.ammo, this.tanks)
   }
 
   add(itemType) {
     switch (itemType) {
       case Item.Bombs:
-        this.hasBombs = true;
-        this.canUseBombs = this.hasMorph;
-        this.canPassBombPassages |= this.canUseBombs;
-        this.canDestroyBombWalls |= this.canPassBombPassages;
-        this.canFly |= this.canUseBombs;
-        break;
       case Item.Morph:
-        this.hasMorph = true;
-        this.canUseBombs = this.hasBombs;
-        this.canUsePowerBombs = this.powerPacks > 0;
-        this.canPassBombPassages = this.canUseBombs || this.canUsePowerBombs;
-        this.canDestroyBombWalls |= this.canPassBombPassages;
-        this.canFly |= this.canUseBombs;
-        break;
       case Item.Gravity:
-        this.hasGravity = true;
-        break;
       case Item.PressureValve:
-        this.hasPressureValve = true;
-        break;
       case Item.HeatShield:
-        this.hasHeatShield = true;
-        break;
       case Item.Varia:
-        this.hasVaria = true;
-        break;
       case Item.HJB:
-        this.hasHiJump = true;
-        break;
       case Item.DoubleJump:
-        this.hasDoubleJump = true;
-        break;
       case Item.SpaceJump:
-        this.hasSpaceJump = true;
-        this.canFly = true;
-        break;
       case Item.ScrewAttack:
-        this.hasScrewAttack = true;
-        this.canDestroyBombWalls = true;
-        break;
       case Item.SpringBall:
-        this.hasSpringBall = true;
-        break;
       case Item.Speed:
-        this.hasSpeed = true;
-        break;
-
       case Item.Ice:
-        this.hasIce = true;
-        break;
       case Item.Wave:
-        this.hasWave = true;
-        break;
       case Item.Charge:
-        this.hasCharge = true;
-        break;
       case Item.Spazer:
-        this.hasSpazer = true;
-        break;
       case Item.Plasma:
-        this.hasPlasma = true;
-        break;
       case Item.Grapple:
-        this.hasGrapple = true;
-        break;
-
-      case Item.Missile:
-        this.missilePacks += 1;
-        this.canOpenRedDoors = true;
-        this.canCrystalFlash =
-          this.missilePacks > 1 && this.superPacks > 1 && this.powerPacks > 2;
-        break;
-      case Item.Super:
-        this.superPacks += 1;
-        this.canOpenGreenDoors = true;
-        this.canOpenRedDoors = true;
-        this.canCrystalFlash =
-          this.missilePacks > 1 && this.superPacks > 1 && this.powerPacks > 2;
-        break;
-      case Item.PowerBomb:
-        this.powerPacks += 1;
-        this.canUsePowerBombs = this.hasMorph;
-        this.canPassBombPassages |= this.canUsePowerBombs;
-        this.canDestroyBombWalls |= this.canPassBombPassages;
-        this.canCrystalFlash =
-          this.missilePacks > 1 && this.superPacks > 1 && this.powerPacks > 2;
-        break;
-
-      case Item.EnergyTank:
-        this.energyTanks += 1;
-        this.totalTanks += 1;
-        break;
-      case Item.Reserve:
-        this.reserveTanks += 1;
-        this.totalTanks += 1;
-        break;
-
       case Item.BeamUpgrade:
       case Item.Xray:
+        this.inventory.push(itemType);
         break;
-
+      case Item.Missile:
+        this.ammo.missilePacks += 1;
+        break;
+      case Item.Super:
+        this.ammo.superPacks += 1;
+        break;
+      case Item.PowerBomb:
+        this.ammo.powerPacks += 1;
+        break;
+      case Item.EnergyTank:
+        this.tanks.energyTanks += 1;
+        break;
+      case Item.Reserve:
+        this.tanks.reserveTanks += 1;
+        break;
       default:
         console.error("[Loadout] Unknown item type:", ItemNames.get(itemType));
         break;
